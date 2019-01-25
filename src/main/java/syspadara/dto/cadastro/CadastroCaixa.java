8d3e1426a5e70002cf89afc220ad7c7873657222
Matheus Jordan <matheusj1599@outlook.com>
2019-01-25 01:16:38 -0200

    Adicionado DTO [Cadastro - Caixa e Venda]

---
+++ src/main/java/syspadara/dto/cadastro/CadastroCaixa.java
@@ -0,0 +1,15 @@
+package syspadara.dto.cadastro;
+
+import java.io.Serializable;
+
+public class CadastroCaixa implements Serializable{
+	private String rg;
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+}
---
+++ src/main/java/syspadara/dto/cadastro/CadastroVenda.java
@@ -0,0 +1,26 @@
+package syspadara.dto.cadastro;
+
+import java.util.List;
+
+//DTO para cadastrar vendas
+public class CadastroVenda {
+	private List<Long> produtos;
+	
+//	private List<Integer> qntds;
+
+	public List<Long> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Long> produtos) {
+		this.produtos = produtos;
+	}
+
+//	public List<Integer> getQntds() {
+//		return qntds;
+//	}
+//
+//	public void setQntds(List<Integer> qntds) {
+//		this.qntds = qntds;
+//	}
+}
